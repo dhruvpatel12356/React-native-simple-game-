dbc664c650c94b63493b6bf62a6637a3529d01e6
Dhruv6995 <156338837+dhruvpatel12356@users.noreply.github.com>
2024-03-25 03:27:11 -0700

    Add files via upload

---
+++ Starter/App.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Modify App.js to make changes</Text>
+    </View>
+  )
+}
+
+export default App
